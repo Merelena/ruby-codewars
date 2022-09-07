4a802db40e8f35a0f392e5a034fa3bed0dd2cb79
Elena Fyodorova <47062507+Merelena@users.noreply.github.com>
2022-09-07 13:53:40 +0300

    AH comments:)

--- Credit Card Mask.rb
+++ Credit Card Mask.rb
@@ -7,13 +7,13 @@
 #
 # Решение - берем все буквы от 0 до -5 и меняем их на #
 def maskify(cc)
-  cc[0..-5] = '#' * cc[0..-5].length
+  cc[0..-5] = '#' * cc[0..-5].length # AH: отличное решение! Не придерешься
   cc
 end
 
 # но в таком случае проходил только первый тест
 # def maskify(cc)
-#   cc[0..-5] = '#' * (cc.length - 4)
+#   cc[0..-5] = '#' * (cc.length - 4) # AH: потому что нельзя определить количество отрицательным числом
 #   cc
 # end
 #
@@ -22,25 +22,25 @@ end
 #
 #реш1
 # def maskify(cc)
-#   cc.size <= 4 ? cc : "#" * (cc.length-4) + cc[-4..-1]
+#   cc.size <= 4 ? cc : "#" * (cc.length-4) + cc[-4..-1] # AH: тут как раз исправлена ошибочка с отрицательным количеством, но операций уже больше, это не гуд
 # end
 #
 # реш2
 # Хотелось бы послушать что тут сделали
 # def maskify(cc)
-#   cc.gsub(/.(?=.{4})/, "#")
+#   cc.gsub(/.(?=.{4})/, "#") 
 # end
 #
 # реш3
 # def maskify(cc)
 #   return cc if cc.length <= 4
-#   cc.split('').fill('#', 0...cc.length - 4).join
+#   cc.split('').fill('#', 0...cc.length - 4).join # AH: .split('') можно заменить на .chars, .fill тут тоже ничего работает
 # end
 #
 # реш4
 # def maskify(cc)
 #   for i in 0..cc.length
-#       cc[i] = '#' unless i>=cc.length-4
+#       cc[i] = '#' unless i>=cc.length-4. # AH: суть в общем-то как и у решений 1, 2 и 3
 #   end
 #   cc
 # end
