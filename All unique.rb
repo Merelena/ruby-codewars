809b1f02a3d007f688a5df9399f1b8e69184dba5
Elena Fyodorova <47062507+Merelena@users.noreply.github.com>
2022-09-08 12:11:28 +0300

    AH comments:)

--- All unique.rb
+++ All unique.rb
@@ -3,19 +3,19 @@
 #   The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
 #
 def unique_chars?(str)
-  str.chars.uniq == str.chars
+  str.chars.uniq == str.chars # AH: хорошее решение. Если помнишь в прошлой задаче вместо .chars использовали .split('')
 end
 
 # Решения codewars
 #
 # Реш 1 - нужна помощь в понимании что тут произошло
 # def unique_chars?(str)
-#   chars = Hash.new(false)
-#   str.each_char do |c|
+#   chars = Hash.new(false) # AH: создали хэшик, в будущем ключами здесь будут выступать буквы, а значениями - true/false
+#   str.each_char do |c| # AH: проходимя по каждому символу
 #     if chars[c]
-#       return false
+#       return false # AH: если в хэше есть ключ = символ c, у которогоо значение = true, значит такая буква уже попадалась и мы вылетаем из функции с false
 #     else
-#       chars[c] = true
+#       chars[c] = true # AH: если же в хэше нет ключа с этим символом, то мы как бы создаем пару ключ значение, к примеру, chars['a'] = true. То есть мы как бы вложили нашу букву в chars и повторений быть не должно
 #     end
 #   end
 #   true
@@ -23,10 +23,10 @@ end
 #
 # Реш 2 - и вот тут
 # def unique_chars?(str)
-#   str.chars.uniq! ? false : true
+#   str.chars.uniq! ? false : true # AH: тут можно было сделать еще круче: !str.chars.uniq!
 # end
 #
 # + Реш 3 с похожей логикой
 # def unique_chars?(str)
-#   str.chars.uniq!.nil?
+#   str.chars.uniq!.nil? # AH: это решение, как и решение 2 основываются на том что возвращает .uniq!. Проэкспериментируй с ним
 # end
